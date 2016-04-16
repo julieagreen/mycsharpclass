3a70b5e66919562507ea4ac4eaeffa68ec991378
julieagreen <julieagreen@JULIEAGREEN4F07>
2016-04-16 15:49:56 -0700

    Add project files.

---
+++ JulieGreenLab2/frmMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace JulieGreenLab2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.lblCard = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Image = ((System.Drawing.Image)(resources.GetObject("pic1.Image")));
+            this.pic1.Location = new System.Drawing.Point(42, 63);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(150, 200);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic2
+            // 
+            this.pic2.Image = ((System.Drawing.Image)(resources.GetObject("pic2.Image")));
+            this.pic2.Location = new System.Drawing.Point(221, 63);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(150, 200);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.pic2_Click);
+            // 
+            // pic3
+            // 
+            this.pic3.Image = ((System.Drawing.Image)(resources.GetObject("pic3.Image")));
+            this.pic3.Location = new System.Drawing.Point(401, 63);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(150, 200);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.pic3_Click);
+            // 
+            // pic4
+            // 
+            this.pic4.Image = ((System.Drawing.Image)(resources.GetObject("pic4.Image")));
+            this.pic4.Location = new System.Drawing.Point(586, 63);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(150, 200);
+            this.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic4.TabIndex = 3;
+            this.pic4.TabStop = false;
+            this.pic4.Click += new System.EventHandler(this.pic4_Click);
+            // 
+            // lblCard
+            // 
+            this.lblCard.BackColor = System.Drawing.Color.LightGray;
+            this.lblCard.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCard.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCard.Location = new System.Drawing.Point(205, 309);
+            this.lblCard.Name = "lblCard";
+            this.lblCard.Size = new System.Drawing.Size(370, 60);
+            this.lblCard.TabIndex = 4;
+            this.lblCard.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(780, 482);
+            this.Controls.Add(this.lblCard);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pick a Card";
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.Label lblCard;
+    }
+}
+
---
+++ JulieGreenLab2/frmMain.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Project:     Lab 2 Ace Descriptions
+ * Programmer:  Julie Green
+ * Date:        April 8, 2016
+ * Purpose:     Displays the type of Ace card selected
+ */
+
+namespace JulieGreenLab2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+            lblCard.Text = "Ace of Spades";
+
+            pic1.BackColor = Color.Black;
+            pic2.BackColor = Color.Transparent;
+            pic3.BackColor = Color.Transparent;
+            pic4.BackColor = Color.Transparent;
+
+            pic1.BorderStyle = BorderStyle.Fixed3D;
+            pic2.BorderStyle = BorderStyle.None;
+            pic3.BorderStyle = BorderStyle.None;
+            pic4.BorderStyle = BorderStyle.None;
+        }
+
+        private void pic2_Click(object sender, EventArgs e)
+        {
+            lblCard.Text = "Ace of Hearts";
+
+            pic1.BackColor = Color.Transparent;
+            pic2.BackColor = Color.Black;
+            pic3.BackColor = Color.Transparent;
+            pic4.BackColor = Color.Transparent;
+
+            pic1.BorderStyle = BorderStyle.None;
+            pic2.BorderStyle = BorderStyle.Fixed3D;
+            pic3.BorderStyle = BorderStyle.None;
+            pic4.BorderStyle = BorderStyle.None;
+        }
+
+        private void pic3_Click(object sender, EventArgs e)
+        {
+            lblCard.Text = "Ace of Clubs";
+
+            pic1.BackColor = Color.Transparent;
+            pic2.BackColor = Color.Transparent;
+            pic3.BackColor = Color.Black;
+            pic4.BackColor = Color.Transparent;
+
+            pic1.BorderStyle = BorderStyle.None;
+            pic2.BorderStyle = BorderStyle.None;
+            pic3.BorderStyle = BorderStyle.Fixed3D;
+            pic4.BorderStyle = BorderStyle.None;
+        }
+
+        private void pic4_Click(object sender, EventArgs e)
+        {
+            lblCard.Text = "Ace of Diamonds";
+
+            pic1.BackColor = Color.Transparent;
+            pic2.BackColor = Color.Transparent;
+            pic3.BackColor = Color.Transparent;
+            pic4.BackColor = Color.Black;
+
+            pic1.BorderStyle = BorderStyle.None;
+            pic2.BorderStyle = BorderStyle.None;
+            pic3.BorderStyle = BorderStyle.None;
+            pic4.BorderStyle = BorderStyle.Fixed3D;
+        }
+    }
+}
